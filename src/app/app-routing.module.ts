a382e443f447747af56d0413319bb1d2e944461e
Alex Schweiz <askibinskiy@gmail.com>
2018-02-28 21:03:54 +0200

    Made some performance improvements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
 import { TeamComponent } from './team/team.component';
 import { ServicesComponent } from './services/services.component';
 import { BlogComponent } from './blog/blog.component';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', loadChildren: './home/home.module#HomeModule' },
   { path: 'about', loadChildren: './about/about.module#AboutModule' },
   { path: 'team', component: TeamComponent },
   { path: 'services', component: ServicesComponent },
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
+import { CoreModule } from '../core/core.module';
+import { HomeRoutingModule } from './home-routing.module';
+
 import { HomeRecentNewsComponent } from './home-recent-news/home-recent-news.component';
 import { HomeWelcomeWordsComponent } from './home-welcome-words/home-welcome-words.component';
 import { HomeOurServicesComponent } from './home-our-services/home-our-services.component';
@@ -8,14 +12,13 @@ import { HomeSliderComponent } from './home-slider/home-slider.component';
 import { HomeHowWeCanHelpComponent } from './home-how-we-can-help/home-how-we-can-help.component';
 import { HomeTestimonialsComponent } from './home-testimonials/home-testimonials.component';
 import { HomeComponent } from './home.component';
-import { SharedModule } from '../shared/shared.module';
-import { CoreModule } from '../core/core.module';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    HomeRoutingModule
   ],
   declarations: [
     HomeSliderComponent,

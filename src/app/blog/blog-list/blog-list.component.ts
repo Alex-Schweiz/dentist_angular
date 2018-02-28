e4285f867d0c0d09c7f59f308ef4eb85b31efb3b
Alex Schweiz <askibinskiy@gmail.com>
2018-02-28 13:13:14 +0200

    Added dynamic blog

--- src/app/blog/blog-list/blog-list.component.ts
+++ src/app/blog/blog-list/blog-list.component.ts
@@ -7,7 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogListComponent implements OnInit {
 
-  blogTitle = 'Latest dental tips (List)';
+  blogArticles = [{
+    title: 'Avoiding bad breath',
+    author: 'John Doe',
+    date: 'May 10, 2016',
+    category: 'Dental',
+    imgPath: 'assets/img/categories-list-01-770x562.jpg',
+    shortDescription: 'Americans oftentimes ignore some basic, daily routines of oral hygiene, that need to be upheld.\n' +
+    '          Practicing healthy habits like these ones will most surely help you avoid such nasty things as a morning\n' +
+    '          bad breath or teeth grinding.',
+    commentsNumber: '31'
+  }, {
+    title: 'Avoiding bad breath',
+    author: 'John Doe',
+    date: 'May 10, 2016',
+    category: 'Dental',
+    imgPath: 'assets/img/categories-list-03-770x562.jpg',
+    shortDescription: 'Americans oftentimes ignore some basic, daily routines of oral hygiene, that need to be upheld.\n' +
+    '          Practicing healthy habits like these ones will most surely help you avoid such nasty things as a morning\n' +
+    '          bad breath or teeth grinding.',
+    commentsNumber: '25'
+  }, {
+    title: 'Avoiding bad breath',
+    author: 'John Doe',
+    date: 'May 10, 2016',
+    category: 'Dental',
+    imgPath: 'assets/img/categories-list-04-770x562.jpg',
+    shortDescription: 'Americans oftentimes ignore some basic, daily routines of oral hygiene, that need to be upheld.\n' +
+    '          Practicing healthy habits like these ones will most surely help you avoid such nasty things as a morning\n' +
+    '          bad breath or teeth grinding.',
+    commentsNumber: '27'
+  }];
 
   constructor() { }
 
